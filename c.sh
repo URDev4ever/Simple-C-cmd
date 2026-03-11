98f30ea3226d7f7a3ef54e8edac612447b64e9aa
URDev <uriel.agustin2010@gmail.com>
2026-03-11 06:31:35 -0300

    Simple C cmd

---
+++ c.sh
@@ -0,0 +1,17 @@
+c() {
+    local file="$1"
+
+    if [[ -z "$file" ]]; then
+        echo "Usage: c file.c"
+        return 1
+    fi
+
+    if [[ ! -f "$file" ]]; then
+        echo "File not found"
+        return 1
+    fi
+
+    local output="${file%.c}"
+
+    gcc "$file" -Wall -Wextra -g -o "$output" && "./$output"
+}
